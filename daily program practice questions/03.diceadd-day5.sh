2121cd48ff6247fed9ef630d066133a2a47aa38a
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-09-25 21:05:19 +0530

    Update 03.diceadd-day5.sh

--- daily program practice questions/03.diceadd-day5.sh
+++ daily program practice questions/03.diceadd-day5.sh
@@ -1,4 +1,4 @@
 
-echo random rolled dice-01 value is $(( x=RANDOM%10 ))
-echo random rolled dice-02 value is $(( y =RANDOM%20 ))
+echo random rolled dice-01 value is $(( x=RANDOM%7-1 ))
+echo random rolled dice-02 value is $(( y =RANDOM%7-1 ))
 echo sum of dice 01 RV and dice 02 RV is $(( $x+$y ));
